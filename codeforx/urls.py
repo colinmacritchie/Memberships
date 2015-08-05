9016adf71962215014b2dfb21b878e908612265a
Colin MacRitchie <ciscopowdercowboy@gmail.com>
2015-08-05 13:15:21 -0600

    updates

--- codeforx/urls.py
+++ codeforx/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
--- codeforx/views.py
+++ codeforx/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 #Functions that displays the home page.
 def home(request):
+    name = "Colin"
+    context = {
+        "the_name": name,
+        }
 
-    return render(request, "base.html", {})
+    return render(request, "base.html", context)
